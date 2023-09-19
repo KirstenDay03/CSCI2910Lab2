48d29db5c634ab9e9b1165d45dac9bdb71eacbde
KirstenDay03 <113709467+KirstenDay03@users.noreply.github.com>
2023-09-19 11:01:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿namespace CSCI2910Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Dictionary<String, List<VideoGame>> games = new Dictionary<String, List<VideoGame>>(); //Creates a dictionary called games
+
+            List<VideoGame> allVG = new List<VideoGame>(); //Creates a list called allVG
+            allVG = p.CreateListOfGames(); //Fills allVG with all the video games in the data
+
+            List<String> platforms = new List<string>(); //Creates a list of strings called platforms
+
+            for(int x = 0; x < allVG.Count; x++) //For loop goes through all the games in allVG and stores each game's platform in platforms
+            {
+                string platform = allVG[x].GetPlatform();
+
+                platforms.Add(platform);
+
+            }
+
+            IEnumerable<String> distinctPlatforms = platforms.Distinct().ToList(); //Takes platforms and removes all the duplicate values
+
+            /**foreach (string plat in distinctPlatforms)
+            {
+                Console.WriteLine(plat);
+            }
+
+            Console.WriteLine();**/
+
+            foreach(string plat in distinctPlatforms)                               //Creates all the lists and adds them to the dictionary using their platform as a key
+            {
+                List<VideoGame> gamesByPlatform = p.CreateList(plat);
+                games.Add(plat, gamesByPlatform);
+            }
+
+            Func<Dictionary<string, List<VideoGame>>, string, List<VideoGame>> getTop5GamesByPlatform = (games, platform) =>        //Tells program what var to expect to send and receive
+            {
+                if (games.TryGetValue(platform, out var platformGames))                                                             //If the platform sent exists as a key, takeout the list associated with that key
+                {
+                    return platformGames.OrderByDescending(game => game.GetGlobalSales()).Take(5).ToList();                         //Return the list in decending order, by global sales, and only take top five
+                }
+                else
+                {
+                    return new List<VideoGame>();                                                                                   //Returns a list if the if fails so that there is no error
+                }
+            };
+
+
+            foreach (string plat in distinctPlatforms)                                  //Displays each list for each platform in distinctPlatforms
+            {
+                Console.WriteLine($"The Top Five Globally Sold Games on {plat}\n" +
+                                  $"------------------------------------------");
+                int counter = 0;
+                var top5GamesForPlatform = getTop5GamesByPlatform(games, plat);
+                foreach (var game in top5GamesForPlatform)
+                {
+                    counter++;
+                    Console.WriteLine($"{counter}: {game}");
+                }
+                Console.WriteLine($"------------------------------------------");
+                Console.WriteLine();
+            }
+
+        }
+
+
+        /// <summary>
+        /// Returns a list that only contains games of a certain platform
+        /// </summary>
+        /// <param name="platform">string platform</param>
+        /// <returns>List<VideoGame> gamesByPlatform</returns>
+        public  List<VideoGame> CreateList(string platform)
+        {
+            List<VideoGame> gamesByPlatform = new List<VideoGame>();                          //creates a new list of VideoGames called games
+            string fileName = $@"..\..\..\DataFile\videogames.csv";                 //saves the relative path to videogames.csv as a string
+
+            try                                                                     //try to validate contained code
+            {
+                using (StreamReader rdr = new StreamReader(fileName))               //opens videogames.csv using StreamReader
+                {
+                    string nextLine = rdr.ReadLine();                               //reads the first line in videogames.csv and saves it in nextLine as a string
+                    while (rdr.Peek() != -1)                                        //while loop to make sure there is another line to read
+                    {
+                        nextLine = rdr.ReadLine();                                  //reads the next line in videogames.csv
+
+                        string[] fields = nextLine.Split(',');                      //creates a string array to hold each individual value in nextLine
+
+                        VideoGame v = new VideoGame(fields[0], fields[1], int.Parse(fields[2]), fields[3], fields[4], double.Parse(fields[5]), double.Parse(fields[6]), double.Parse(fields[7]), double.Parse(fields[8]), double.Parse(fields[9]));
+                        //creates a new VideoGame v and uses the values stored in fields as parameters
+
+                        if (v.GetPlatform() == platform)
+                        {
+                            gamesByPlatform.Add(v);                                               //adds VideoGame v to games
+                        }
+                    }//End While
+
+                    rdr.Close();                                                    //closes StreamReader rdr
+                }//End using
+            }//End Try
+            catch (Exception e)                                                      //catches any exception that could be thrown
+            {
+                Console.WriteLine(e.Message);                                       //displays the error message
+            }
+
+            return gamesByPlatform;
+        }
+
+        /// <summary>
+        /// Returns a list of all the games in the dataset
+        /// </summary>
+        /// <returns>List<VideoGame> games</returns>
+        public List<VideoGame> CreateListOfGames()
+        {
+            List<VideoGame> games = new List<VideoGame>();                          //creates a new list of VideoGames called games
+            string fileName = $@"..\..\..\DataFile\videogames.csv";                 //saves the relative path to videogames.csv as a string
+
+            try                                                                     //try to validate contained code
+            {
+                using (StreamReader rdr = new StreamReader(fileName))               //opens videogames.csv using StreamReader
+                {
+                    string nextLine = rdr.ReadLine();                               //reads the first line in videogames.csv and saves it in nextLine as a string
+                    while (rdr.Peek() != -1)                                        //while loop to make sure there is another line to read
+                    {
+                        nextLine = rdr.ReadLine();                                  //reads the next line in videogames.csv
+
+                        string[] fields = nextLine.Split(',');                      //creates a string array to hold each individual value in nextLine
+
+                        VideoGame v = new VideoGame(fields[0], fields[1], int.Parse(fields[2]), fields[3], fields[4], double.Parse(fields[5]), double.Parse(fields[6]), double.Parse(fields[7]), double.Parse(fields[8]), double.Parse(fields[9]));
+                        //creates a new VideoGame v and uses the values stored in fields as parameters
+
+                        
+                            games.Add(v);                                               //adds VideoGame v to games
+                        
+                    }//End While
+
+                    rdr.Close();                                                    //closes StreamReader rdr
+                }//End using
+            }//End Try
+            catch (Exception e)                                                      //catches any exception that could be thrown
+            {
+                Console.WriteLine(e.Message);                                       //displays the error message
+            }
+
+            return games;
+        }
+
+
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI2910Lab2
+{
+    internal class VideoGame : IComparable<VideoGame>
+    {
+        //Fields
+        private string name;
+        private string platform;
+        private int year;
+        private string genre;
+        private string publisher;
+        private double na_sales;
+        private double eu_sales;
+        private double jp_sales;
+        private double other_sales;
+        private double global_sales;
+
+        //Constructors
+        /// <summary>
+        /// Base Constructor, sets all fields to default values
+        /// </summary>
+        public VideoGame()
+        {
+            name = "";
+            platform = "";
+            year = 0;
+            genre = "";
+            publisher = "";
+            na_sales = 0.0;
+            eu_sales = 0.0;
+            jp_sales = 0.0;
+            other_sales = 0.0;
+            global_sales = 0.0;
+        }
+
+        /// <summary>
+        /// Parameterized Constructor, sets fields to the passed values
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="platform"></param>
+        /// <param name="year"></param>
+        /// <param name="genre"></param>
+        /// <param name="publisher"></param>
+        /// <param name="na_sales"></param>
+        /// <param name="eu_sales"></param>
+        /// <param name="jp_sales"></param>
+        /// <param name="other_sales"></param>
+        /// <param name="global_sales"></param>
+        public VideoGame(string name, string platform, int year, string genre, string publisher, double na_sales, double eu_sales, double jp_sales, double other_sales, double global_sales)
+        {
+            this.name = name;
+            this.platform = platform;
+            this.year = year;
+            this.genre = genre;
+            this.publisher = publisher;
+            this.na_sales = na_sales;
+            this.eu_sales = eu_sales;
+            this.jp_sales = jp_sales;
+            this.other_sales = other_sales;
+            this.global_sales = global_sales;
+        }
+
+        //Getters
+        /// <summary>
+        /// GetPublisher(), returns publisher
+        /// </summary>
+        /// <returns>string publisher</returns>
+        public string GetPublisher()
+        {
+            return publisher;
+        }
+
+        /// <summary>
+        /// GetGenre(), returns genre
+        /// </summary>
+        /// <returns>string genre</returns>
+        public string GetGenre()
+        {
+            return genre;
+        }
+
+        /// <summary>
+        /// GetPlatform(), returns platform
+        /// </summary>
+        /// <returns>string platform</returns>
+        public string GetPlatform()
+        {
+            return platform;
+        }
+
+        public double GetGlobalSales()
+        {
+            return global_sales;
+        }
+
+        /// <summary>
+        /// CompareTo(), method implemented from IComparable. Compares one VideoGame object to another VideoGame object ,and then sorts them.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>int</returns>
+        public int CompareTo(VideoGame other)
+        {
+            if (this.name.CompareTo(other.name) < 0)
+            {
+                return -1;
+            }
+            else if (this.name.CompareTo(other.name) == 0)
+            {
+                if (this.year.CompareTo(other.year) < 0)
+                {
+                    return -1;
+                }
+                else if (this.year.CompareTo(other.year) == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// ToString(), displays VideoGame objects in a formatted way as a string
+        /// </summary>
+        /// <returns>string message</returns>
+        public override String ToString()
+        {
+            string message;
+
+            message = $"{name}, {year}, {genre}\n" +
+                      $"    Published by {publisher} on {platform}.\n" +
+                      $"    Global Sales: ${global_sales}";
+
+            return message;
+        }
+    }
+}
